24478c611f226ff8cca232a65f6bc3106550443f
samitman <si237@njit.edu>
2020-09-15 12:51:11 -0400

    Java Data Homework

---
+++ Basics/src/JavaDataHW.java
@@ -0,0 +1,11 @@
+
+public class JavaDataHW {
+	public static void main(String[] args) {
+		String name = "Samuel";
+		System.out.println(name);
+		int age = 19;
+		System.out.println(age);
+		float number = 1.1f;
+		System.out.println(number);
+	}
+}
